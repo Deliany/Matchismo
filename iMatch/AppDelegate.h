051c9e89e8c5624a9e2e65cf5df00c0f29d9fe6f
Deliany Delirium <Deliany@Delianys-Mac-mini.local>
2013-02-02 22:02:51 +0200

    Initial Commit

---
+++ iMatch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iMatch
+//
+//  Created by Deliany Delirium on 02.02.13.
+//  Copyright (c) 2013 Clear Sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iMatch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iMatch
+//
+//  Created by Deliany Delirium on 02.02.13.
+//  Copyright (c) 2013 Clear Sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
